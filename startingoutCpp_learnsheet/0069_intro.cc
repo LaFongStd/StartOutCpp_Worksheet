d722eb2e97b8a4adebb8eb461f4e95851ab19d3f
LaFong <104591906+LaFongStd@users.noreply.github.com>
2023-06-05 18:48:57 +0800

    Add files via upload

---
+++ startingoutCpp_learnsheet/0069_intro.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x =3;
+
+    cout << "There are " << x << " zombies. " << endl;
+
+    return 0;
+}
---
+++ startingoutCpp_learnsheet/0077_intro.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int sword, shield, bow;
+    sword = 1200;
+    shield =2400;
+    bow = 900;
+
+    cout << "I buy a sword for " << sword << ", a shield for " << shield << " and a bow for " << bow  << "." << endl;
+
+    return 0; 
+
+}
